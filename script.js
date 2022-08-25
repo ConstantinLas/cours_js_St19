17b2f5194313617ba2f5c0a9bef9b885680f5c9a
Constantin <constantinlasker@Constantins-Air.fritz.box>
2022-08-26 00:17:17 +0200

    lesson_19 modifited

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 'use strict';
 
-
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
-console.log(numberOfFilms);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ' ');
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: [],
+    geners: {},
     privat: true
 };
 
-let quastian = [];
-let filmScore = [];
-quastian[0] =  prompt("Один из последних просмотренных фильмов?'", " ");
-filmScore[0] = prompt("На сколько оцените его?", " ");
+let a = prompt('Один из последних просмотренных фильмов?', ' ');
+let b  = prompt('На сколько оцените его?', ' ');
+let c = prompt('Один из последних просмотренных фильмов?', ' ');
+let d  = prompt('На сколько оцените его?', ' ');
 
-quastian[1] =  prompt("Один из последних просмотренных фильмов?'", " ");
-filmScore[1] = prompt("На сколько оцените его?", " ");
-
-personalMovieDB.movies[quastian[0]] = filmScore[0];
-personalMovieDB.movies[quastian[1]] = filmScore[1];
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
